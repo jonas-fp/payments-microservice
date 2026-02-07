3f9dd502143d2cd2243a5d294d363e83b5a0d49c
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-02-06 18:33:49 -0800

    Initializr Spring Boot skeleton with hello endpoint

---
+++ src/main/java/com/payment_processing_system/payment_processing_system/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.payment_processing_system.payment_processing_system.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping({ "/", "/hello" })
+    public String hello() {
+        return "Hello from payment-processing-system";
+    }
+}
