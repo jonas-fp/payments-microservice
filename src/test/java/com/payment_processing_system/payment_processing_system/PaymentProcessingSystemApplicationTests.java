85ca9ef37d0d1c4960c96a18088a028a13c22bc8
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-02-12 17:58:52 -0800

    Set up Testcontainers
    This commit enables the dev to run the mvn test command without having to start, stop, and clean up containers.

--- src/test/java/com/payment_processing_system/payment_processing_system/PaymentProcessingSystemApplicationTests.java
+++ src/test/java/com/payment_processing_system/payment_processing_system/PaymentProcessingSystemApplicationTests.java
@@ -2,9 +2,22 @@ package com.payment_processing_system.payment_processing_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class PaymentProcessingSystemApplicationTests {
+
+	@Container
+	@ServiceConnection
+	@SuppressWarnings("resource")
+	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+			.withDatabaseName("payments")
+			.withUsername("payments")
+			.withPassword("payments");
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/payment_processing_system/payment_processing_system/web/HelloControllerTest.java
+++ src/test/java/com/payment_processing_system/payment_processing_system/web/HelloControllerTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,7 +16,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 class HelloControllerTest {
+
+    @Container
+    @ServiceConnection
+    @SuppressWarnings("resource")
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("payments")
+            .withUsername("payments")
+            .withPassword("payments");
 
     @Autowired
     private MockMvc mockMvc;
