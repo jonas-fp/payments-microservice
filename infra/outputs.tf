e3fe7d53d56abc7ccb9c67279de899d3b9be0227
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-04-03 15:36:36 -0700

    Set up infra with Terraform

---
+++ infra/outputs.tf
@@ -0,0 +1,11 @@
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.app_cluster.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app_service.name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.address
+}
