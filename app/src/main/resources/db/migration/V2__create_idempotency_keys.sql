5c0d1356d6616968cd58b527c962d6182d6b1a79
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-04-14 15:42:16 -0700

    Add customer_id column
    So that the idempotency key can be scoped to the customer, which helps the app avoid returning the wrong information

--- app/src/main/resources/db/migration/V2__create_idempotency_keys.sql
+++ app/src/main/resources/db/migration/V2__create_idempotency_keys.sql
@@ -1,6 +1,7 @@
 CREATE TABLE idempotency_keys (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     action_type VARCHAR(128) NOT NULL,
+    customer_id VARCHAR(128) NOT NULL,
     idempotency_key VARCHAR(255) NOT NULL,
     payload_hash VARCHAR(128) NOT NULL,
     resource_type VARCHAR(64),
@@ -11,7 +12,8 @@ CREATE TABLE idempotency_keys (
     -- DESIGN: ensures that any action on the same endpoint with the same 
     -- idempotency key is only executed once.
     CONSTRAINT uk_idempotency_keys_scope_key UNIQUE (
-        endpoint_scope, idempotency_key
+        customer_id, idempotency_key, action_type
+    ),
     ),
     CONSTRAINT chk_resource_type CHECK (
         resource_type in ('AUTHORIZE', 'CAPTURE', 'REFUND')
