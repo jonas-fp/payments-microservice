3e3c8a0bb1496604c06ed058402c7f3d37eaa97f
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-04-17 17:52:18 -0700

    Ensure each capture and refund success event has a capture or refund

--- app/src/main/resources/db/migration/V3__create_payment_events.sql
+++ app/src/main/resources/db/migration/V3__create_payment_events.sql
@@ -37,3 +37,29 @@ CREATE UNIQUE INDEX uk_payment_events_processor_event_reference
 
 CREATE INDEX idx_payment_events_payment_id 
     ON payment_events(payment_id);
+
+-- Ensure that capture and refund success events always have a corresponding
+-- record in the captures or refunds table
+CREATE OR REPLACE FUNCTION validate_event_has_capture_or_refund_record()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (NEW.event_type = 'CAPTURE_SUCCESS') THEN
+        IF NOT EXISTS (SELECT 1 FROM captures WHERE event_id = NEW.id) THEN
+            RAISE EXCEPTION 'CAPTURE_SUCCESS event must have a corresponding ' 
+                            'record in captures table.';
+        END IF;
+    ELSIF (NEW.event_type = 'REFUND_SUCCESS') THEN
+        IF NOT EXISTS (SELECT 1 FROM refunds WHERE event_id = NEW.id) THEN
+            RAISE EXCEPTION 'REFUND_SUCCESS event must have a corresponding ' 
+                            'record in refunds table.';
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE CONSTRAINT TRIGGER trg_check_event_has_capture_or_refund_record
+AFTER INSERT ON payment_events
+DEFERRABLE INITIALLY DEFERRED
+FOR EACH ROW
+EXECUTE FUNCTION validate_event_has_capture_or_refund_record();
