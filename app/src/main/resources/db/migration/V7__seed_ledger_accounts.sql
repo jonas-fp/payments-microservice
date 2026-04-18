a2bc3be4be5ada369a1af3c542c667719d92641b
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-04-17 18:23:00 -0700

    Add ledger accounts to database

---
+++ app/src/main/resources/db/migration/V7__seed_ledger_accounts.sql
@@ -0,0 +1,14 @@
+INSERT INTO ledger_accounts (id, account_code, account_name, account_type)
+VALUES
+    (
+        '9d96ee97-3526-43a0-914c-190e2fb575c4',
+        '10001',
+        'Cash Clearing',
+        'ASSET'
+    ),
+    (
+        'fbee20bf-64dd-4c15-9ca5-8f03b79e554a',
+        '20002',
+        'Deferred Revenue',
+        'LIABILITY'
+    );
