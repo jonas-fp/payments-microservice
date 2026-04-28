b2a2533015377a026a2cee407038a0f5a9d2c9da
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-04-27 17:48:49 -0700

    Add query to find idempotency key

--- app/src/main/java/com/payment_processing_system/payment_processing_system/repository/IdempotencyKeyRepository.java
+++ app/src/main/java/com/payment_processing_system/payment_processing_system/repository/IdempotencyKeyRepository.java
@@ -1,5 +1,6 @@
 package com.payment_processing_system.payment_processing_system.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import com.payment_processing_system.payment_processing_system.entity.IdempotencyKeyEntity;
@@ -7,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IdempotencyKeyRepository
         extends JpaRepository<IdempotencyKeyEntity, UUID> {
+
+    Optional<IdempotencyKeyEntity> findByCustomerIdAndIdempotencyKeyAndActionType(
+            String customerId, String idempotencyKey, String actionType);
 }
