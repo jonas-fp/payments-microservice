5813cbca200756871ec14549aebb8844478a3edb
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-05-05 16:10:06 -0700

    Add ProcessorStatementRowRepository

---
+++ app/src/main/java/com/payment_processing_system/payment_processing_system/reconciliation/infra/ProcessorStatementRowRepository.java
@@ -0,0 +1,10 @@
+package com.payment_processing_system.payment_processing_system.reconciliation.infra;
+
+import com.payment_processing_system.payment_processing_system.reconciliation.domain.ProcessorStatementRow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface ProcessorStatementRowRepository extends JpaRepository<ProcessorStatementRow, UUID> {
+}
