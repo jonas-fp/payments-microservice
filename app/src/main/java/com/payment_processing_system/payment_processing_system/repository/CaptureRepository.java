baafc86881eb3b376b42632c66d05f4664ec141b
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-05-06 15:37:38 -0700

    Add query to CaptureRepository

--- app/src/main/java/com/payment_processing_system/payment_processing_system/repository/CaptureRepository.java
+++ app/src/main/java/com/payment_processing_system/payment_processing_system/repository/CaptureRepository.java
@@ -1,9 +1,17 @@
 package com.payment_processing_system.payment_processing_system.repository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.payment_processing_system.payment_processing_system.entity.CaptureEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CaptureRepository extends JpaRepository<CaptureEntity, UUID> {
+    Optional<CaptureEntity> findByProcessorCaptureReference(
+        String processorCaptureReference);
+
+    List<CaptureEntity> findAllByCreatedAtBetween(OffsetDateTime start,
+        OffsetDateTime end);
 }
