1872ed147f6e32c1b74e2bee609b4e44d88f20d9
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-05-06 15:38:58 -0700

    Add queries to RefundRepository

--- app/src/main/java/com/payment_processing_system/payment_processing_system/repository/RefundRepository.java
+++ app/src/main/java/com/payment_processing_system/payment_processing_system/repository/RefundRepository.java
@@ -1,9 +1,17 @@
 package com.payment_processing_system.payment_processing_system.repository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.payment_processing_system.payment_processing_system.entity.RefundEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefundRepository extends JpaRepository<RefundEntity, UUID> {
+    Optional<RefundEntity> findByProcessorRefundReference(
+        String processorRefundReference);
+
+    List<RefundEntity> findAllByCreatedAtBetween(OffsetDateTime start,
+        OffsetDateTime end);
 }
