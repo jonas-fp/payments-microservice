6cc0766f63028c10d706b514ca3ea7f39fa3ac6d
Jonas Field-Patton <jfieldpatton@gmail.com>
2026-05-06 15:34:19 -0700

    Add query to ProcessorStatementRowRepository

--- app/src/main/java/com/payment_processing_system/payment_processing_system/reconciliation/infra/ProcessorStatementRowRepository.java
+++ app/src/main/java/com/payment_processing_system/payment_processing_system/reconciliation/infra/ProcessorStatementRowRepository.java
@@ -3,9 +3,13 @@ package com.payment_processing_system.payment_processing_system.reconciliation.i
 import com.payment_processing_system.payment_processing_system.reconciliation.domain.ProcessorStatementRow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProcessorStatementRowRepository
     extends JpaRepository<ProcessorStatementRow, UUID> {
+    List<ProcessorStatementRow> findAllByReconciliationRunId(
+        UUID reconciliationRunId);
 }
